48f5077f5a861d983ad3405b282e35f5b1fa0bd1
Justin Vititoe <jvit1985@yahoo.com>
2022-06-23 17:44:07 -0500

    created thoughts and user models

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        //must match a valid email address(look into Mongoose's matching validation)
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
